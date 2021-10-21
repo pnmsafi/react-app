9083cd08ab561503fea87ced227d2a6f0c6c37d2
pnmsafi <pnmsafi@gmail.com>
2021-10-21 11:58:21 +0530

    REACT

---
+++ src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,105 @@
+import "./rightbar.css";
+import { Users } from "../../dummyData";
+import Online from "../online/Online";
+
+export default function Rightbar({ profile }) {
+  const HomeRightbar = () => {
+    return (
+      <>
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
+          </span>
+        </div>
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          {Users.map((u) => (
+            <Online key={u.id} user={u} />
+          ))}
+        </ul>
+      </>
+    );
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">Mumbai</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">India</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Status:</span>
+            <span className="rightbarInfoValue">VIP</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">User friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/1.jpg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">hobie</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/2.jfif"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">jimin</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/3.jpg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">kapil</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/4.jpg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">bharati</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/5.jfif"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">abmitabh</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/6.jfif"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">aish</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        {profile ? <ProfileRightbar /> : <HomeRightbar />}
+      </div>
+    </div>
+  );
+}
